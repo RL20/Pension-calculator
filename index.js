7515f566582153dc52cdd95cc7ff9f79f4f8cab1
harel <harel_shalom@yahoo.com>
2024-03-08 04:26:57 +0200

    fix : displayed Deposit Fee Percentage based on the maximum amount

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ const calculatePension = async () => {
   let hasMaxFee = false;
   let maxFeeAmount = 0;
   const PensionMonthlyDeposit = salary * 0.20830434782608695; // 20.83%
-
   moneyAccumulate = parseFloat(await askQuestion("Enter the amount of money accumulated : "));
   // depositFeePercentage = parseFloat(await askQuestion("Enter deposit fee percentage (e.g., enter 1 for 1%): "));
   depositFeePercentage = parseFloat(await askQuestion("Enter deposit fee percentage (e.g., enter 1 for 1%): "));
+  let displayedDepositFeePercentage = depositFeePercentage;
+
   if (depositFeePercentage > 0) {
     const hasMaxFeeResponse = await askQuestion("Is there a maximum fee for deposit? (yes/no): ");
     if (hasMaxFeeResponse.toLowerCase() === "yes") {
       hasMaxFee = true;
       maxFeeAmount = parseFloat(await askQuestion("Enter the maximum fee amount: "));
+    }
+    // If there is a maximum amount for the deposit fee
+    if (hasMaxFee) {
+      // Calculate the appropriate percentage based on the maximum amount
+      displayedDepositFeePercentage = (maxFeeAmount / (PensionMonthlyDeposit / 100)).toFixed(4);
     }
   }
   cumulativeFee = parseFloat(await askQuestion("Enter cumulative fee percentage: "));
@@ -46,7 +52,7 @@ const calculatePension = async () => {
 
   console.log(`**********************************`);
   console.log(`Cumulative Fee: ${cumulativeFee}%`);
-  console.log(`Deposit Fee: ${depositFeePercentage}%`);
+  console.log(`Deposit Fee: ${displayedDepositFeePercentage}%`);
   console.log("Accumulate: ", moneyAccumulate);
 
   rl.close(); // Close the interface
